c3b07001243b160607033994bc4be3165323f1bd
Amit Bind <amitbind1947@gmail.com>
2026-01-08 10:47:18 +0530

    fixed bugs

--- src/services/base.ts
+++ src/services/base.ts
@@ -7,7 +7,7 @@ export const baseApi = createApi({
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).user.token;
       if (token) {
-        headers.set('Authorization', `Bearer ${token}`);
+        headers.set('X-Authorization', `Bearer ${token}`);
       }
       return headers;
     },
